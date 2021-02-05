9f1f41795c61823743fadb52ee486ac8a0707339
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2021-02-05 16:32:58 +0900

    feat(AnatomyOfBasis): enable to update a text view with a random value between 1 and 6

--- DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,9 +2,15 @@ package com.example.android.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * DiceRoller demonstrates simple interactivity in an Android app.
+ * It contains one button that updates a text view with a random
+ * value between 1 and 6.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +20,12 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Click listener for the Roll button.
+     */
     private fun rollDice() {
         Toast.makeText(this, "button clocked", Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = (1..6).random().toString()
     }
 }
