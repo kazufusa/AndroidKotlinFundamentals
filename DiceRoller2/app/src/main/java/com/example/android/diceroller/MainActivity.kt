cb07a59b9954985ce7e93b5ee815e55bf2595840
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2021-02-06 23:14:28 +0900

    feat(ImageResources): add image resources

--- DiceRoller2/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ DiceRoller2/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.android.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -25,7 +26,15 @@ class MainActivity : AppCompatActivity() {
      */
     private fun rollDice() {
         Toast.makeText(this, "button clocked", Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = (1..6).random().toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
