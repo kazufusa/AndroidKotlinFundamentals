c5b03a967a51c233d542efa7262b6bb1a4230624
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2021-02-09 14:51:17 +0900

    feat(2.4.DataBinding): use data binding to display data

--- AboutMeDataBinding/app/src/main/java/com/android/example/aboutme/MainActivity.kt
+++ AboutMeDataBinding/app/src/main/java/com/android/example/aboutme/MainActivity.kt
@@ -9,18 +9,22 @@ import androidx.databinding.DataBindingUtil
 import com.android.example.aboutme.databinding.ActivityMainBinding
 
 /**
- * MainActivity of the AboutMe app. This app demonstrated:
- *      * Getting user input with an EditText.
+ * Main Activity of the AboutMe app. This app demonstrates:
+ *      * LinearLayout with TextViews, ImageView, Button, EditText, and ScrollView
+ *      * Getting user input with scrollable text
  *      * Click handler for a Button to retrieve text from an EditText and set it in a TextView.
- *      * Setting a click handler on a TextView.
  *      * Setting the visibility status of a view.
+ *      * Data binding between MainActivity and activity_main.xml. How to remove findViewById,
+ *        and how to display data in views using the data binding object.
  */
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Aleks Haecky")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
 
         binding.doneButton.setOnClickListener {
             addNickname()
@@ -38,7 +42,8 @@ class MainActivity : AppCompatActivity() {
      */
     private fun addNickname() {
         binding.apply {
-            nicknameText.text = nicknameEdit.text.toString()
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
             nicknameText.visibility = View.VISIBLE
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
